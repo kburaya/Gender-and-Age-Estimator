ff25e27ceb95ca705aeeb6eccc3a153114e54c3d
Kseniya.Buraya <kseniya.buraya@billing.ru>
2016-08-08 15:29:44 +0300

    Messages receiving added

--- Source/server.py
+++ Source/server.py
@@ -1,23 +1,28 @@
-#!/usr/bin/env python
 import pika
 import time
 
+
 def callback(ch, method, properties, body):
-    print (" [x] Received %r" % (body,))
-    #TODO message processing here!
-    print (" [x] Done")
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    print(" [x] Received %r" % (body,))
+    # TODO message processing here!
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost'))
+        host='localhost'))
     channel = connection.channel()
 
     channel.queue_declare(queue='messages_queue', durable=True)
-    print (' [*] Waiting for messages. To exit press CTRL+C')
+    print(' [*] Waiting for messages. To exit press CTRL+C')
 
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(callback,
                           queue='messages_queue')
 
     channel.start_consuming()
+
+
+if __name__ == "__main__":
+    main()
